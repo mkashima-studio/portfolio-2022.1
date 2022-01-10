3fd5e9eac93a2ba6c8ce3bece1476ddf983bbc9c
Maisa <maisakashima@gmail.com>
2022-01-10 14:01:33 -0300

    Mobile Navbar ok

--- pages/layout.js
+++ pages/layout.js
@@ -1,17 +1,39 @@
+import { useState } from "react";
 import Head from 'next/head'
 import Footer from '../app/components/modules/footer'
 import Navbar from '../app/components/modules/navBar'
 import layout from './layout.module.css'
+import Link from "next/link";
 
 export default function Layout({children}) {
+
+    const [isOpen,setIsOpen] = useState(false);
+    const openMenu = () => setIsOpen(!isOpen);
+
     return (
         <>
             <Head>
                 <title>mkashima</title>
                 <link rel="favicon" href="public/favicon.ico" />
-                <link rel= "preload" href="/fonts/goudosi.otf" as="font" crossOrigin="" />
+                <link rel="preload" href="/fonts/goudosi.otf" as="font" crossOrigin="" />
             </Head>
-            <Navbar className={layout.nav} />
+            <header>
+                <Navbar className={isOpen === false ? layout.nav : layout.nav +' '+layout.active} />
+                <button className={isOpen === false ? layout.hamburger : layout.hamburger +' '+layout.active}
+                onClick={openMenu}>
+                    <span className={layout.bar}></span>
+                    <span className={layout.bar}></span>
+                    <span className={layout.bar}></span>
+                </button>
+                <ul className={isOpen === false ? layout.navmob : layout.navmob +' '+layout.active} 
+                onClick={openMenu}>
+                    <li className={layout.navmobitem}><Link href="/"><a>/ HOME</a></Link></li>
+                    <li className={layout.navmobitem}><Link href="/fotografia"><a>/ FOTOGRAFIA</a></Link></li>
+                    <li className={layout.navmobitem}><Link href="/webDev"><a>/ WEB DEV</a></Link></li>
+                    <li className={layout.navmobitem}><Link href="/info"><a>/ INFO</a></Link></li>
+                    <li className={layout.navmobitem}><Link href="/contato"><a>/ CONTATO</a></Link></li>
+                </ul>
+            </header>
             <section className={layout.main}>
                 {children}
             </section>
