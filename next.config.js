d05ad5773f39ca11526dc75eacbaaaa7624a177f
Maisa <maisakashima@gmail.com>
2022-01-10 23:59:25 -0300

    npm export part 1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
+const debug = process.env.NODE_ENV !== 'production'
+
 module.exports = {
   reactStrictMode: true,
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
+  assetPrefix: !debug ? '/portfolio-2022.1/' : '',
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' },
+      '/info': { page: '/info' },
+      '/fotografia': { page: '/fotografia' },
+      '/contato': { page: '/contato' },
+      '/webDev': { page: '/webDev' },
+    }
   },
 }
